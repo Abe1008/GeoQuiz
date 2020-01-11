85b51edecbaabbe0cdfb4823732c3130a4ea5556
Abe1008 <abe1008@gmail.com>
2020-01-11 15:53:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -0,0 +1,19 @@
+package com.example.geoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// http://www.bignerdranch.com/solutions/AndroidProgramming3e.zip
+// https://www.bignerdranch.com/books/android-programming-the-big-nerd-ranch-guide-4th/
+// .
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+        //
+    }
+}
